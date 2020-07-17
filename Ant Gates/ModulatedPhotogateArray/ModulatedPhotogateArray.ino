ff41e94c98bdd8d57c4f0ceac0bb8953f66e7c59
Andrew Quitmeyer <andrew.quitmeyer@gmail.com>
2020-07-17 13:36:36 -0500

    Update ModulatedPhotogateArray.ino
    Nice sensing of ants!

--- Ant Gates/ModulatedPhotogateArray/ModulatedPhotogateArray.ino
+++ Ant Gates/ModulatedPhotogateArray/ModulatedPhotogateArray.ino
@@ -1,3 +1,5 @@
+#include <MovingAverage.h>
+
 /*
    Modulated PhotoGate
 
@@ -8,21 +10,26 @@
 
 
 int sensorPins[] = {A0, A1, A2, A3, A4};
-#define totalSensors = 5; // this number needs to match the number of entries above
+#define totalSensors 5 // this number needs to match the number of entries above
 
-int rawReadings[totalSensors];
 int readingHIGH[totalSensors];
-int readingLOW[totalSensors];
+int readingLOW[totalSensors]; //Just measuring ambient light
+int diffReading[totalSensors];
+int avgReading[totalSensors];
 
 
+int thresholds[] = {10, 10, 10, 10, 10}; //Need same number of thresholds as pins! can tweak individual thresholds
+int detected[] = { -1, -1, -1, -1, -1}; //Need same number of thresholds as pins!
+
+MovingAverage average[totalSensors](50);
 
 int LEDPins[] = {12, 11, 10, 9, 8};
-#define totalLEDs = 5; // this number needs to match the number of entries above
+#define totalLEDs 5 // this number needs to match the number of entries above
 
 
 
 void setup() {
-
+  Serial.begin(57600);
   //Initialize all sensors connected
   for (int i = 0; i < totalSensors; i++) {
     pinMode(sensorPins[i], INPUT);
@@ -38,9 +45,67 @@ void setup() {
 
   }
 
-
 }
 
 void loop() {
+
+
+  //First turn all the LEDs OFF
+  for (int i = 0; i < totalLEDs; i++) {
+    digitalWrite(LEDPins[i], LOW);   // turn the LED on (HIGH is the voltage level)
+  }
+  delay(10);//if using photo resistors you need some delay to let the sensors charge and discharge
+
+  //measure all their values (with ambient light)
+  for (int i = 0; i < totalSensors; i++) {
+
+    readingLOW[i] = analogRead(sensorPins[i]);
+  }
+
+
+  //NEXT turn all the LEDs on
+  for (int i = 0; i < totalLEDs; i++) {
+    digitalWrite(LEDPins[i], HIGH);   // turn the LED on (HIGH is the voltage level)
+  }
+  delay(1);//if using photo resistors you need some delay to let the sensors charge and discharge
+
+  //measure all their values (with OUR light)
+  for (int i = 0; i < totalSensors; i++) {
+
+    readingHIGH[i] = analogRead(sensorPins[i]);
+
+    //Do some calculations
+    diffReading[i] = readingHIGH[i] - readingLOW[i];
+    if (diffReading[i] < thresholds[i]) {
+      detected[i] = 1; // We detected something in front of the gate! something is blocking the light!
+    }
+    else {
+      detected[i] = 0; // nope the light value is normal
+    }
+
+    String title = "Sensor";
+    String valueSplitter = ",";
+    //Print out all the values
+    //Serial.print(title+i+valueSplitter+diffReading[i]+valueSplitter);
+    //Serial.print(readingHIGH[i]+valueSplitter);
+
+    //Display the average reading
+    average[i].update(diffReading[i]);
+    //Serial.print(title + i + valueSplitter + average[i].get() + valueSplitter);
+
+
+    //Show the difference between the average, and the instantaneous new reading
+    float instant = abs(average[i].get() - diffReading[i]);
+    Serial.print(title + i + valueSplitter + instant + valueSplitter);
+
+  }
+
+
+  Serial.println( );
+
+
+
+
+
 
 }
