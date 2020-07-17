73ef6ecfa800f0ac2330d67addcf8aa4d8005278
Andrew Quitmeyer <andrew.quitmeyer@gmail.com>
2020-07-17 16:01:11 -0500

    Updated print commands for better serialplotter

--- Ant Gates/ModulatedPhotogateArray_LEDsensors/ModulatedPhotogateArray_LEDsensors.ino
+++ Ant Gates/ModulatedPhotogateArray_LEDsensors/ModulatedPhotogateArray_LEDsensors.ino
@@ -1,7 +1,7 @@
 #include <MovingAverage.h>
 
 /*
-   Modulated PhotoGate
+   Modulated PhotoGate with LED as sensor
 
    basic photogate for an arduino where we pulse the light and measure the difference between on and off
    for constant re-calibration and better performance across varying ambient light levels
@@ -10,7 +10,7 @@
 
 
 int sensorPins[] = {A0, A1, A2, A3, A4};
-#define totalSensors 5 // this number needs to match the number of entries above
+#define totalSensors 2 // this number needs to match the number of entries above
 
 int readingHIGH[totalSensors];
 int readingLOW[totalSensors]; //Just measuring ambient light
@@ -21,10 +21,10 @@ int avgReading[totalSensors];
 int thresholds[] = {10, 10, 10, 10, 10}; //Need same number of thresholds as pins! can tweak individual thresholds
 int detected[] = { -1, -1, -1, -1, -1}; //Need same number of thresholds as pins!
 
-MovingAverage average[totalSensors](50);
+MovingAverage average[totalSensors](100);
 
 int LEDPins[] = {12, 11, 10, 9, 8};
-#define totalLEDs 5 // this number needs to match the number of entries above
+#define totalLEDs 2 // this number needs to match the number of entries above
 
 
 
@@ -54,12 +54,13 @@ void loop() {
   for (int i = 0; i < totalLEDs; i++) {
     digitalWrite(LEDPins[i], LOW);   // turn the LED on (HIGH is the voltage level)
   }
-  delay(10);//if using photo resistors you need some delay to let the sensors charge and discharge
-
+  delay(3);//if using photo resistors you need some delay to let the sensors charge and discharge
+//delayMicroseconds(1000); // for LED as sensor these delays need to be the same when on and off // LED as sensor doesn't seem to work well at times shorter than 1000 micros
   //measure all their values (with ambient light)
   for (int i = 0; i < totalSensors; i++) {
 
-    readingLOW[i] = analogRead(sensorPins[i]);
+    readingLOW[i] = readLED(1, sensorPins[i]);
+    
   }
 
 
@@ -67,12 +68,13 @@ void loop() {
   for (int i = 0; i < totalLEDs; i++) {
     digitalWrite(LEDPins[i], HIGH);   // turn the LED on (HIGH is the voltage level)
   }
-  delay(1);//if using photo resistors you need some delay to let the sensors charge and discharge
+  delay(3);//if using photo resistors you need some delay to let the sensors charge and discharge
+//delayMicroseconds(1000);
 
   //measure all their values (with OUR light)
   for (int i = 0; i < totalSensors; i++) {
 
-    readingHIGH[i] = analogRead(sensorPins[i]);
+    readingHIGH[i] =  readLED(1, sensorPins[i]);
 
     //Do some calculations
     diffReading[i] = readingHIGH[i] - readingLOW[i];
@@ -84,7 +86,6 @@ void loop() {
     }
 
     String title = "Sensor";
-    
     String valueSplitter = ",";
     //Print out all the values
     //Serial.print(title+i+":"+diffReading[i]+valueSplitter);
@@ -92,21 +93,25 @@ void loop() {
 
     //Display the average reading
     average[i].update(diffReading[i]);
-    //Serial.print(title + i + ":" + average[i].get() + valueSplitter);
+    //Serial.print(title + i + ":"+ average[i].get() + valueSplitter);
 
 
     //Show the difference between the average, and the instantaneous new reading
     float instant = abs(average[i].get() - diffReading[i]);
-    Serial.print(title + i + ":" + instant + valueSplitter);
+    Serial.print(title + i + ":"+ instant + valueSplitter);
 
   }
 
 
   Serial.println( );
 
+}
 
-
-
-
-
+int readLED(int numReadings, int port) {            // Read analog value n times and avarage over those n times
+  int total = 0;
+  for(int x = 0; x < numReadings; x++) {
+    total += analogRead(port);
+    //delay(5);
+  }
+  return total/numReadings;
 }
